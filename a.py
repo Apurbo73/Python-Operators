e361d7081a949083d182fa668934f945eee6f8e2
Apurbo Deb Nath <apurbodebnath50@gmail.com>
2025-07-30 17:48:27 +0600

    done

--- a.py
+++ a.py
@@ -6,3 +6,14 @@
 # Parentheses has the highest precedence, meaning that expressions inside parentheses must be evaluated first:
 
 print((6 + 3) - (6 + 3))
+
+# Parenthesis have the highest precedence, and need to be evaluated first.
+# The calculation above reads 9 - 9 = 0
+
+
+print(100 + 5 * 3)
+
+# """
+# Multiplication has higher precedence than addition, and needs to be evaluated first.
+# The calculation above reads 100 + 15 = 115
+# """
